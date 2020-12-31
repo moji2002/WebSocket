8ae167f91e0643f1b2bfaa379b3f1f0167d12517
Mojtaba <moji2002@gmail.com>
2020-12-31 20:18:40 +0330

    price

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const axios = require('axios');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const axios = require('axios');
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
@@ -34,9 +34,6 @@ io.on('connection', (socket) => {
 
     });
 });
-
-
-
 
 http.listen(port, () => {
     console.log('listening on *:' + port);
