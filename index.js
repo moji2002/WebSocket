c9d8e155325579bf923f6b20105d9013a543e35f
Mojtaba <moji2002@gmail.com>
2020-12-31 20:17:44 +0330

    price

--- index.js
+++ index.js
@@ -21,12 +21,15 @@ getPrices();
 setInterval(getPrices, 5000);
 
 io.on('connection', (socket) => {
-    console.log('connection established');
-
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 
     socket.on('price', (value) => {
         // socket.emit('price', value + ' server'); emit to single connection
         // io.emit('price', value); // emit to all connection
+        // socket.broadcast.emit('price', value); // emit to all connection except current one
         socket.emit('price', prices[value]); //emit to single connection;
 
     });
